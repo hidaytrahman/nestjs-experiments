43264f0c80e1ac6f233083f61ebe12943ae58d36
Hidayt Rahman <hidaytrahman@gmail.com>
2023-12-27 20:43:21 +0530

    chore: integrate HttpModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GithubModule } from './apps/github/github.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [GithubModule],
+  imports: [GithubModule, HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/apps/github/github.module.ts
+++ src/apps/github/github.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { GithubController } from "./github.controller";
 import { GithubServices } from "./github.services";
+import { HttpModule } from "@nestjs/axios"
 
 @Module({
-    controllers:[GithubController],
-    providers: [GithubServices]
+    controllers: [GithubController],
+    providers: [GithubServices],
+    imports: [HttpModule]
 })
 export class GithubModule {
     constructor() {
