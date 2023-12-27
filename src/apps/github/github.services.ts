12ae8e0842e48704e4aa2764773712447d3e0de4
Hidayt Rahman <hidaytrahman@gmail.com>
2023-12-27 21:38:46 +0530

    feat: add repo and followers list

--- src/apps/github/github.services.ts
+++ src/apps/github/github.services.ts
@@ -1,13 +1,15 @@
 import { HttpService } from "@nestjs/axios";
 import { Injectable } from "@nestjs/common";
 import { lastValueFrom, map } from "rxjs";
+import { API_BASE_URL } from "src/config/api.config";
+import { filteredGithubProfile, filteredGithubRepo } from "./utils";
+import { getFilteredObject } from "src/shared/utils";
+
 
 @Injectable()
 export class GithubServices {
 
-    constructor(private readonly httpService: HttpService) {
-
-    }
+    constructor(private readonly httpService: HttpService) { }
 
     getUser(username: string) {
         return {
@@ -15,20 +17,57 @@ export class GithubServices {
         }
     }
 
+    // fetch data from github and modify it in one api
     async getUserDetails(username: string) {
-        const getGithubProfile = this.httpService.get(`https://api.github.com/users/hidaytrahman`)
+
+        // fetch the data from github profile
+        const getProfile = this.httpService.get(`${API_BASE_URL.GITHUB}users/${username}`)
             .pipe(
                 map(response => response.data)
             );
 
 
-        const githubProfileData = await lastValueFrom(getGithubProfile);
+        // fetch the data from github profile
+        const getRepoList = this.httpService.get(`${API_BASE_URL.GITHUB}users/${username}/repos?per_page=5`)
+            .pipe(
+                map(response => response.data)
+            );
 
-        console.log(githubProfileData)
 
+        // convert promise into object
+        const githubProfile = await lastValueFrom(getProfile);
+        const githubRepoList = await lastValueFrom(getRepoList);
+
+        // fetch the followers from github user if follower exists
+        let githubFollowerList = [];
+        if (githubProfile.followers > 0) {
+            const getFollowerList = this.httpService.get(`${API_BASE_URL.GITHUB}users/${username}/followers`)
+                .pipe(
+                    map(response => response.data)
+                );
+
+            // convert promise into object
+            githubFollowerList = await lastValueFrom(getFollowerList);
+        }
+
+
+
+
+        // filter fields from each repo from the list
+        const filteredFieldsRepo = githubRepoList.map((object) => getFilteredObject(filteredGithubRepo, object))
+
+
+        // console.log(githubProfileData)
+        console.log(githubFollowerList)
+        // console.log(getFilteredObject(filteredGithubProfile, githubProfileData))
+
+
+        // modify the response and finalise it for the user
         return {
             message: `User details has been fectched for ${username}`,
-            ...githubProfileData
+            ...getFilteredObject(filteredGithubProfile, githubProfile),
+            repos: filteredFieldsRepo,
+            followers: githubFollowerList
         }
     }
 }
